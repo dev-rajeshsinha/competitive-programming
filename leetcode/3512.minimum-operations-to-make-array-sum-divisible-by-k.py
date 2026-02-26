d61b3eb27094f8b4d1b08e155a3ca2a76fd23950
Rajesh Sinha <rajeshsinha97@outlook.com>
2026-02-26 22:22:44 +0530

    02-26-2026

---
+++ leetcode/3512.minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -0,0 +1,97 @@
+#
+# @lc app=leetcode id=3512 lang=python3
+#
+# [3512] Minimum Operations to Make Array Sum Divisible by K
+#
+# https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/description/
+#
+# algorithms
+# Easy (92.39%)
+# Likes:    321
+# Dislikes: 36
+# Total Accepted:    178.9K
+# Total Submissions: 193.7K
+# Testcase Example:  '[3,9,7]\n5'
+#
+# You are given an integer array nums and an integer k. You can perform the
+# following operation any number of times:
+#
+#
+# Select an index i and replace nums[i] with nums[i] - 1.
+#
+#
+# Return the minimum number of operations required to make the sum of the array
+# divisible by k.
+#
+#
+# Example 1:
+#
+#
+# Input: nums = [3,9,7], k = 5
+#
+# Output: 4
+#
+# Explanation:
+#
+#
+# Perform 4 operations on nums[1] = 9. Now, nums = [3, 5, 7].
+# The sum is 15, which is divisible by 5.
+#
+#
+#
+# Example 2:
+#
+#
+# Input: nums = [4,1,3], k = 4
+#
+# Output: 0
+#
+# Explanation:
+#
+#
+# The sum is 8, which is already divisible by 4. Hence, no operations are
+# needed.
+#
+#
+#
+# Example 3:
+#
+#
+# Input: nums = [3,2], k = 6
+#
+# Output: 5
+#
+# Explanation:
+#
+#
+# Perform 3 operations on nums[0] = 3 and 2 operations on nums[1] = 2. Now,
+# nums = [0, 0].
+# The sum is 0, which is divisible by 6.
+#
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= nums.length <= 1000
+# 1 <= nums[i] <= 1000
+# 1 <= k <= 100
+#
+#
+#
+
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        # In each operation, we can decrease an element by 1. To make the sum of the array divisible by k, we need to find the remainder of the sum when divided by k. The number of operations needed will be equal to this remainder, because we can decrease any element to reduce the sum by 1 in each operation.
+        # Time complexity: O(n) where n is the length of the input array
+        # Space complexity: O(1) since we are using a constant amount of space
+        return sum(nums) % k
+
+
+# @lc code=end
